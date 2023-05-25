f75256c8a64b7f869173c35b647d608386264fb5
nuffQuantumSayd <aaronkornish.ak@gmail.com>
2023-05-25 11:47:03 -0700

    Created processRequest function

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 window.onload = function () {
     var pictureBtn = $("picture-button");
-    pictureBtn.onclick = getRandomPic;
+    pictureBtn.onclick = main;
 };
-function getRandomPic() {
-    console.log("Button was clicked");
+function main() {
+    var http = new XMLHttpRequest();
+    http.open("GET", "https://dog.ceo/api/breeds/image/random");
+    http.onreadystatechange = processRequest;
+    http.send();
+}
+function processRequest() {
+    var http = this;
+    if (http.readyState == 4 && http.status == 200) {
+        alert("Check the console because we are finished!");
+        console.log(http.responseText);
+    }
 }
 function $(id) {
     return document.getElementById(id);
--- ts/main.ts
+++ ts/main.ts
@@ -1,11 +1,36 @@
 window.onload = function() {
     let pictureBtn = $("picture-button");
-    pictureBtn.onclick = getRandomPic;
+    pictureBtn.onclick = main;
+    
 }
 
-function getRandomPic() {
-    console.log("Button was clicked");
+function main() {
+    //console.log("Button was clicked");
+    let http = new XMLHttpRequest();
+    //prepare the request to the server
+    //GET request asks the server for data
+    //The URL is the website we are requesting data from
+    http.open("GET", "https://dog.ceo/api/breeds/image/random");
+
+    //function to handle different ready states
+    http.onreadystatechange = processRequest;
+
+
+
+    //Send the request to the server
+    http.send();
 }
+
+function processRequest() {
+    let http = <XMLHttpRequest>this;
+
+    if(http.readyState == 4 && http.status == 200){
+        alert("Check the console because we are finished!");
+        console.log(http.responseText);
+    }
+    
+}
+
 
 function $(id:string):HTMLElement{
     return document.getElementById(id);
